c19f6b05591990dd0229a5e5bb36e56f7208d251
rulamon <ruben.lamon@gmail.com>
2018-11-18 22:36:34 +0100

    simplified with split by :, thanks to Nico De Gols

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 function toSeconds(str) {
-	let timeArr = str.split("");
-	let secondHalf = parseInt(timeArr.splice(timeArr.length-2, 2).join(""));
-	let firstHalf;
-	if (typeof timeArr[1] === ":"){
-		firstHalf = parseInt(timeArr[0]) * 60;
-	} else {
-		firstHalf = parseInt(timeArr.splice(0,2).join("")) * 60;
-	};
+	let timeArr = str.split(":");
+	let firstHalf = parseInt(timeArr[0]) * 60;
+	let secondHalf = parseInt(timeArr[1]);
 	return firstHalf + secondHalf;
 };
+
 function toKmPerHour(str) {
 	if (/^\d{0,2}:\d{2}$/.test(str)) {
 		return Math.round((1 / toSeconds(str) * 3600) * 100) / 100 + "km/h";
@@ -16,4 +12,4 @@ function toKmPerHour(str) {
 		return "Input format must be mm:ss of m:ss"
 	};
 }
-console.log(toKmPerHour("5:14"));
+console.log(toKmPerHour("5:00"));
